d78d0b89a3ddbebae093ce00cac406ef79a9afe0
blueswen <blueswen.tw@gmail.com>
2026-01-18 18:17:02 +0800

    Update swagger-ui-dist to 5.31.0

--- update-swagger-ui.sh
+++ update-swagger-ui.sh
@@ -1,8 +1,18 @@
+SWAGGER_UI_VERSION=${1}
 DEST="./mkdocs_swagger_ui_tag/swagger-ui"
 SOURCE="./node_modules/swagger-ui-dist"
 JS_LIST="swagger-ui-bundle.js.map swagger-ui-standalone-preset.js.map swagger-ui-bundle.js swagger-ui-standalone-preset.js"
 CSS_LIST="swagger-ui.css swagger-ui.css.map"
-HTML_LIST="oauth2-redirect.html"
+OAUTH2_LIST="oauth2-redirect.html oauth2-redirect.js"
+
+if [ -z "${SWAGGER_UI_VERSION}" ]; then
+    echo "Please provide a Swagger UI version as the first argument."
+    exit 1
+fi
+
+rm -rf ${SOURCE}
+
+npm install swagger-ui-dist@${SWAGGER_UI_VERSION}
 
 for f in $JS_LIST; do
     cp ${SOURCE}/${f} ${DEST}/javascripts/${f}
@@ -12,6 +22,6 @@ for f in $CSS_LIST; do
     cp ${SOURCE}/${f} ${DEST}/stylesheets/${f}
 done
 
-for f in $HTML_LIST; do
+for f in $OAUTH2_LIST; do
     cp ${SOURCE}/${f} ${DEST}/${f}
 done
